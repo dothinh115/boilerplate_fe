b900b4a659030a37580b7d2c0fcf2bc458ea6ccd
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-05-30 17:29:18 +0700

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,11 +16,12 @@ export default defineNuxtConfig({
           rel: "stylesheet",
         },
       ],
+
       meta: [
         {
           name: "viewport",
           content:
-            "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0",
+            "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
         },
       ],
       title: "Control Panel",
