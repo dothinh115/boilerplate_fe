a9d94afb7b1f73a5fd86bd26f4593a88c85284d9
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-07-26 20:52:46 +0700

    update directive

--- plugins/clickOutside.ts
+++ plugins/clickOutside.ts
@@ -2,13 +2,18 @@ export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.directive("click-outside", {
     mounted(el, binding) {
       el.clickOutSideEvent = (event: MouseEvent) => {
-        if (el !== event.target && !el.contains(event.target))
+        if (el !== event.target && !el.contains(event.target)) {
           binding.value(event);
+        }
       };
-      document.body.addEventListener("click", (e) => el.clickOutSideEvent(e));
+      document.body.addEventListener("mousedown", (e) =>
+        el.clickOutSideEvent(e)
+      );
     },
     unmounted(el) {
-      document.body.addEventListener("click", (e) => el.clickOutSideEvent(e));
+      document.body.addEventListener("mousedown", (e) =>
+        el.clickOutSideEvent(e)
+      );
     },
   });
 });
