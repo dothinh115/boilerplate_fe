0feccaa7b6e218b8fb0f7c6f515e35d8b480c828
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-07-28 22:40:15 +0700

    update

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,8 +3,7 @@ module.exports = {
     {
       name: "truyenhot-cp",
       script: "./.output/server/index.mjs",
-      instances: 2,
-      exec_mode: "cluster",
+      instances: 1,
       autorestart: true,
       watch: false,
       env_production: {
