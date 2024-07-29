f1c8382df705bc430734ad8f184d91ec8d04351f
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-07-29 16:22:47 +0700

    update

--- components/Confirm.vue
+++ components/Confirm.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="bg-gray-100 rounded-[5px] w-fit mx-auto">
+  <div class="bg-gray-100 rounded-[5px] w-fit mx-auto overflow-hidden">
     <div class="border-b border-gray-500 p-2 text-center space-y-4">
       <slot name="icon"></slot>
       <div>{{ message }}</div>
     </div>
-    <div class="p-2 flex space-x-2 justify-center">
-      <button class="btn btn-green btn-icon flex-1" @click="confirm">
+    <div class="flex">
+      <button
+        class="btn-icon flex-1 p-2 bg-emerald-600 space-x-2 text-gray-50 lg:hover:bg-emerald-700 duration-200"
+        @click="confirm"
+      >
         <i class="fa-solid fa-check"></i><span>Xác nhận</span>
       </button>
-      <button class="btn btn-red btn-icon flex-1" @click="cancel">
+      <button
+        class="btn-icon flex-1 p-2 bg-red-500 space-x-2 text-gray-50 lg:hover:bg-red-700 duration-200"
+        @click="cancel"
+      >
         <i class="fa-solid fa-xmark"></i><span>Huỷ</span>
       </button>
     </div>
