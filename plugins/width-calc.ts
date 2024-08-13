d0c01799c0a050ea98427397edbd9abfc6d7e53b
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-08-13 08:22:23 +0700

    update

--- plugins/width-calc.ts
+++ plugins/width-calc.ts
@@ -3,14 +3,19 @@ export default defineNuxtPlugin(() => {
     provide: {
       widthCalc: (widthObj: { [key: string]: number }) => {
         let result: any = {};
+        const { screenWidth } = useGetState();
         watchEffect(() => {
           const perChar = 12;
           for (const [key, value] of Object.entries(widthObj)) {
             let w = value * perChar;
             if (w < 80) w = 80;
-            else if (w > 200) {
+            else if (w > 300) {
               if (key === "id") w = 120;
-              else w = 200;
+              else if (screenWidth.value <= 375) {
+                w = 190;
+              } else if (screenWidth.value > 375 && screenWidth.value <= 430) {
+                w = 210;
+              } else w = 300;
             }
 
             result[key] = w;
