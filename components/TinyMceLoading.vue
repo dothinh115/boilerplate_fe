f849caa6fbb9e554a7df783ed0f386e73f595ac8
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-08-14 20:21:04 +0700

    update

--- components/TinyMceLoading.vue
+++ components/TinyMceLoading.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="max-w-full mx-auto">
-    <div class="space-y-4">
-      <div class="h-40 skeleton"></div>
-      <div class="h-6 skeleton w-3/4"></div>
-      <div class="h-6 skeleton w-1/2"></div>
-    </div>
+    <div class="h-56 skeleton"></div>
   </div>
 </template>
