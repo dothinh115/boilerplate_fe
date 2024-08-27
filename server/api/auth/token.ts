14f38d07ccfefff617746bd492b60def58fc2c32
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-08-27 16:48:30 +0700

    update

--- server/api/auth/token.ts
+++ server/api/auth/token.ts
@@ -1,6 +1,5 @@
-import { H3Event, setCookie, sendRedirect } from "h3";
-import { joinURL, withQuery } from "ufo";
-import type { QueryObject } from "ufo";
+import { H3Event, setCookie } from "h3";
+import { joinURL } from "ufo";
 import {
   REFRESH_TOKEN,
   ACCESS_TOKEN,
@@ -8,29 +7,13 @@ import {
 } from "@/utils/constants";
 import { jwtDecode } from "jwt-decode";
 
-const parseQueryString = (
-  reqUrl: string | undefined,
-  baseUrl: string
-): QueryObject => {
-  const url = new URL(reqUrl || "", baseUrl);
-  const queryObject: QueryObject = {};
-  const params = new URLSearchParams(url.searchParams);
-  params.forEach((value, key) => {
-    queryObject[key] = value;
-  });
-  return queryObject;
-};
-
 export default defineEventHandler(async (event: H3Event) => {
   const { apiUrl, cookiePath } = useRuntimeConfig().public; // Lấy api thực từ env
   const baseUrl = event.node.req.headers.host;
   const protocol = event.node.req.headers["x-forwarded-proto"] || "http";
   const url = `${protocol}://${baseUrl}`;
-  const queryObject = parseQueryString(url, apiUrl); //phân tích lấy query string từ url
   const replacedPath = event.path.replace(/^\/api\//, ""); // Bỏ prefix /api
-  const target = queryObject
-    ? withQuery(joinURL(apiUrl, replacedPath), queryObject)
-    : joinURL(apiUrl, replacedPath);
+  const target = joinURL(apiUrl, replacedPath);
 
   return await proxyRequest(event, target, {
     async onResponse(event, response) {
