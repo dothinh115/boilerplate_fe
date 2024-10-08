3d1f3f4ee7dd287f3df6b6f09a1a4a38c6a95875
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-10-08 22:02:25 +0700

    update

--- server/api/me.ts
+++ server/api/me.ts
@@ -1,35 +1,20 @@
 import { joinURL } from "ufo";
 import { H3Event } from "h3";
-import {
-  ACCESS_TOKEN,
-  REFRESH_TOKEN,
-  TOKEN_EXPIRED_TIME,
-} from "@/utils/constants";
-import { isTokenValid } from "~/utils/functions/common";
+import { ACCESS_TOKEN } from "@/utils/constants";
 
 export default defineEventHandler(async (event: H3Event) => {
   const { apiUrl } = useRuntimeConfig().private;
   const replacedPath = event.path.replace(/^\/api\//, "");
   const target = joinURL(apiUrl, replacedPath);
-  const accessToken = getCookie(event, ACCESS_TOKEN);
-  const refreshToken = getCookie(event, REFRESH_TOKEN);
-  const expTime = getCookie(event, TOKEN_EXPIRED_TIME);
-  const isValid = isTokenValid(expTime ?? null);
-  if (!accessToken || !refreshToken || !isValid) return;
-  let headers: Record<string, string> = {};
-  Object.entries(event.node.req.headers).forEach(([key, value]) => {
-    if (
-      ["host", "content-length", "connection", "accept-encoding"].includes(key)
-    ) {
-      return;
-    }
-    if (typeof value === "string") {
-      headers[key] = value;
-    } else if (Array.isArray(value)) {
-      headers[key] = value.join(",");
-    }
-  });
+  const accessToken =
+    event.node.req.headers["new-token"] ?? getCookie(event, ACCESS_TOKEN);
+  if (!accessToken) return;
+
   return await proxyRequest(event, target, {
-    headers,
+    ...(accessToken && {
+      headers: {
+        authorization: "Bearer " + accessToken,
+      },
+    }),
   });
 });
